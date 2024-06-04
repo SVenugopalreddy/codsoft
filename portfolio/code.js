014a5a2f4b4c3b92e042c762d6fc6e2d36203401
SVenugopalreddy <121606405+SVenugopalreddy@users.noreply.github.com>
2024-06-04 20:58:42 +0530

    Add files via upload

---
+++ portfolio/code.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted! We will get back to you soon.');
+});
